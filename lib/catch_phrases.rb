05eefdc80f83d736fb99423eff988f1babb423be
Seohee  Im <seoheeim@vt.edu>
2020-07-21 15:20:21 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,19 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
+mario 
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase2 = "It's Dangerous To Go Alone! Take This."
+  puts phrase2
+end 
+phrase2
+
+def all_phrases
+  puts 
